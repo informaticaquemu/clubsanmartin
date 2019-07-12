86d5cb2db21a435508c3eb30dd1be963ab20b057
unknown <altolaguirre.francisco.tecnico@gmail.com>
2019-07-12 10:23:28 -0300

    agregando interfaces y fotos

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Club San Martin - Quemu Quemu</title>
-</head>
-<body>
-    <h1>Bienvenidos!</h1>
-</body>
-</html>
+<?php header("Location: views/inicio.php");?>
---
+++ views/inicio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Club San Martin - Quemu Quemu</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/fontawesome.min.css">
+    <link rel="stylesheet" href="../css/all.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm sticky-top navbar-dark bg-primary">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">Club San Martin</a>
+
+        <!-- Links -->
+        <ul class="navbar-nav">
+        <li class="nav-item">
+                <a class="nav-link" href="">Inicio</a> 
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=""></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href=""></a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
+
+    </div>
+
+<script src="../../js/jquery.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/popper.js"></script>
+    <script src="../../js/ctacte/functions.js"></script>
+    <script src="../../js/ctacte/clientes.js"></script>
+</body>
+</html>
